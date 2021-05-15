06e8f85f816b1187d0a6b582182a5387832fc402
Oleg Sivokon <olegsivokon@gmail.com>
2021-05-15 22:24:03 +0300

    Added example with saving to file

---
+++ examples/save_to_file.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+set -ex
+
+texel-detect-frozen \
+    -o example.json \
+    https://storage.googleapis.com/hiring_process_data/freeze_frame_input_a.mp4 \
+    https://storage.googleapis.com/hiring_process_data/freeze_frame_input_b.mp4 \
+    https://storage.googleapis.com/hiring_process_data/freeze_frame_input_c.mp4
